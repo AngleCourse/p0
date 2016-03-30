93044ee1b9e65d009d732f86e0c2118aaa2ea964
AngleNet <wangmin1123@iie.ac.cn>
2016-03-30 22:24:18 +0800

    Add feature

--- src/github.com/cmu440/p0/server_impl.go
+++ src/github.com/cmu440/p0/server_impl.go
@@ -6,19 +6,22 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"sync"
 )
 
 const (
-	ServerPort = 9999
+	NumClientBuffer = 100
 )
 
 type multiEchoServer struct {
 	// TODO: implement this!
 	isStarted   bool      //Signifies whether the server is started or not
 	isClosed    bool      // Signifies whether the server is closed or not
-	numConn     uint      // Used to count the number of active clients
+	numConnChan chan int  // Used to count the number of active clients
 	exitChannel chan bool // Used to end the dispatcher
 	listenConn  net.Listener
+	wg          sync.WaitGroup      // A global waiter
+	clientChans map[int]chan string // List of all client channels
 }
 
 // New creates and returns (but does not start) a new MultiEchoServer.
@@ -27,8 +30,9 @@ func New() MultiEchoServer {
 	return &multiEchoServer{
 		isStarted:   false,
 		isClosed:    false,
-		numConn:     0,
+		numConnChan: make(chan int),
 		exitChannel: make(chan bool),
+		clientChans: make(map[int]chan string),
 	}
 }
 
@@ -39,9 +43,10 @@ func (mes *multiEchoServer) Start(port int) error {
 		return errors.New("Error: Server has been closed.")
 	}
 	if mes.listenConn, err = net.Listen("tcp",
-		fmt.Sprintf(":%d", ServerPort)); err != nil {
+		fmt.Sprintf(":%d", port)); err != nil {
 		return err
 	}
+	mes.numConnChan <- 0
 	go mes.dispatch()
 	mes.isStarted = true
 	return nil
@@ -53,40 +58,89 @@ func (mes *multiEchoServer) Start(port int) error {
 // "exit" from server, it stops all of its connections
 // immediately.
 func (mes *multiEchoServer) dispatch() {
-    for {
-        select {
-        case status := <- exitChannel:
-            if status{
-            }
-        }
-    default:
-        conn, err := mes.listenConn.Accept()
-        if err != nil{
-            fmt.Println("\tError on accept %vth connection: %v", 
-            mes.numConn + 1, err)
-        }else{
-            go handleConn(conn)
-            mes.numConn++
-        }
-    }
+
+	mes.wg.Add(1)
+
+	for {
+		select {
+		case status := <-mes.exitChannel:
+			if status {
+				// Tell other connection handlers to die
+
+				// terminate itself
+				defer mes.wg.Done()
+				return
+			}
+		default:
+			conn, err := mes.listenConn.Accept()
+			if err != nil {
+				fmt.Println("\tError on accept connection: %v",
+					err)
+			} else {
+				go mes.handleConn(conn)
+			}
+		}
+	}
 }
 
-func (mes *multiEchoServer) handleConn(conn net
+/**
+ * Handles every connection independently.
+ */
+func (mes *multiEchoServer) handleConn(conn net.Conn) {
+	// After doing all works, remeber to "done" the waiter.
+	defer mes.wg.Done()
+	defer mes.deCount()
+	mes.wg.Add(1)
+	count := <-mes.numConnChan
+	count++
+	mes.numConnChan <- count
+	mes.clientChans[count+1] = make(chan string, NumClientBuffer)
+	for {
+		select {
+		case message := <-mes.clientChans[count]:
+			conn.Write([]byte(message))
+		}
+	}
+
+}
+
+func (mes *multiEchoServer) deCount() {
+	count := <-mes.numConnChan
+	count--
+	mes.numConnChan <- count
+}
+
+func (mes *multiEchoServer) incCount() {
+	count := <-mes.numConnChan
+	count++
+	mes.numConnChan <- count
+}
 
 func (mes *multiEchoServer) Close() {
 	// TODO: implement this!
-    if isStarted {
-        mes.exitChannel <- true
-    }else if isClosed{
-        fmt.Println("\tThe server has been closed.")
-    }else{
-        fmt.Println("\tThe server is not started.")
-    }
+	if mes.isStarted {
+		mes.exitChannel <- true
+		mes.wg.Wait()
+
+		// Flags "isStarted" and "isClosed" are only set by
+		// server's close and start operations.
+		mes.isStarted = false
+		mes.isClosed = true
+		close(mes.exitChannel)
+		close(mes.numConnChan)
+		fmt.Println("\tNow the server is down.")
+	} else if mes.isClosed {
+		fmt.Println("\tThe server has been closed.")
+	} else {
+		fmt.Println("\tThe server is not started.")
+	}
 }
 
 func (mes *multiEchoServer) Count() int {
 	// TODO: implement this!
-    return mes.numConn;
+	count := <-mes.numConnChan
+	mes.numConnChan <- count
+	return count
 }
 
 // TODO: add additional methods/functions below!
