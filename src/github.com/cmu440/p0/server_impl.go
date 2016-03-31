f437543e082935affc84ec83624179a515bd0af3
AngleNet <wangmin1123@iie.ac.cn>
2016-03-31 18:27:33 +0800

    Fix some bugs

--- src/github.com/cmu440/p0/server_impl.go
+++ src/github.com/cmu440/p0/server_impl.go
@@ -5,19 +5,32 @@ package p0
 import (
 	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net"
+	"os"
 	"sync"
-    "io"
 )
 
 const (
 	NumClientBuffer = 100
+	Debug           = true
 )
+
+var logger *log.Logger
+
+// Package initialization function
+func init() {
+	if Debug {
+		logger = log.New(os.Stdout, "", log.Lshortfile)
+	}
+}
 
 type multiEchoServer struct {
 	// TODO: implement this!
 	isStarted   bool      //Signifies whether the server is started or not
 	isClosed    bool      // Signifies whether the server is closed or not
+	serverExit  bool      //Broadcast to all service channels to exit
 	numConnChan chan int  // Used to count the number of active servers
 	exitChannel chan bool // Used to end the dispatcher
 	listenConn  net.Listener
@@ -31,7 +44,8 @@ func New() MultiEchoServer {
 	return &multiEchoServer{
 		isStarted:   false,
 		isClosed:    false,
-		numConnChan: make(chan int),
+		serverExit:  false,
+		numConnChan: make(chan int, 1),
 		exitChannel: make(chan bool),
 		serverChans: make(map[int]chan string),
 	}
@@ -39,6 +53,7 @@ func New() MultiEchoServer {
 
 func (mes *multiEchoServer) Start(port int) error {
 	// TODO: implement this!
+	fuck("red", "We are starting the Server")
 	var err error
 	if mes.isClosed {
 		return errors.New("Error: Server has been closed.")
@@ -48,8 +63,10 @@ func (mes *multiEchoServer) Start(port int) error {
 		return err
 	}
 	mes.numConnChan <- 0
+	fuck("red", "Gonna start dispatcher")
 	go mes.dispatch()
 	mes.isStarted = true
+	fuck("red", "Start server successfully")
 	return nil
 }
 
@@ -61,23 +78,28 @@ func (mes *multiEchoServer) Start(port int) error {
 func (mes *multiEchoServer) dispatch() {
 
 	mes.wg.Add(1)
+	fuck("red", "We are starting the Dispatcher")
 
 	for {
 		select {
 		case status := <-mes.exitChannel:
+			fuck("cyan", fmt.Sprintf("Got exit message: %v", status))
 			if status {
 				// Tell other connection handlers to die
-
+				mes.serverExit = true
 				// terminate itself
 				defer mes.wg.Done()
 				return
 			}
 		default:
+			fuck("cyan", "Waiting for connection...")
 			conn, err := mes.listenConn.Accept()
+			fuck("cyan", "Got an connection")
 			if err != nil {
 				fmt.Println("\tError on accept connection: %v",
 					err)
 			} else {
+				fuck("red", "Gonna fork a handleConn")
 				go mes.handleConn(conn)
 			}
 		}
@@ -90,53 +112,68 @@ func (mes *multiEchoServer) dispatch() {
 func (mes *multiEchoServer) handleConn(conn net.Conn) {
 	// After doing all works, remeber to "done" the waiter.
 	defer mes.wg.Done()
-    defer conn.Close()
-	defer mes.deCount()
-    defer delete(mes.serverChans, count)
-	mes.wg.Add(1)
-    serverRead := make(chan string)
-    clientExit := make(chan bool)
-    go readFromClient(conn, serverRead, clientExit)
+	defer conn.Close()
+	serverRead := make(chan string)
+	clientExit := make(chan bool)
+	go readFromClient(conn, serverRead, clientExit)
 	count := <-mes.numConnChan
 	count++
 	mes.numConnChan <- count
-	mes.serverChans[count+1] = make(chan string, NumClientBuffer)
-	for !mes.serverExit{
+	fuck("cyan", fmt.Sprintf("Handling %vth connection", count))
+	defer mes.deCount()
+	mes.wg.Add(1)
+	mes.serverChans[count] = make(chan string, NumClientBuffer)
+	defer delete(mes.serverChans, count)
+	for !mes.serverExit {
 		select {
 		case message := <-mes.serverChans[count]:
-            if _, err := conn.Write([]byte(message)); err == io.EOF{
-                return
-            }
-        // For now, the server can only cache one message.
-        case message := <-serverRead:
-            select {
-                case <- clientExit:
-                    return
-                default:
-                    for _, channel := range mes.serverChans{
-                if len(channel) == cap(channel){
-                    <- channel
-                }
-                channel <- message
-            }
+			fuck("red", fmt.Sprintf("%v receieved:%s",
+				count, message))
+			if _, err := conn.Write([]byte(message)); err == io.EOF {
+				fuck("cyan", fmt.Sprintf("%v gonna Exit", count))
+				return
+			}
+		// For now, the server can only cache one message.
+		case message := <-serverRead:
+			select {
+			case exit := <-clientExit:
+				fuck("cyan",
+					fmt.Sprintf("client %v send an exit(%v)",
+						count, exit))
+				if exit {
+					fuck("red",
+						fmt.Sprintf("Gonna lose %v connection.",
+							count))
+					return
+				}
+			default:
+				for _, channel := range mes.serverChans {
+					fuck("red", fmt.Sprintf("Client %v sends: %s",
+						count, message))
+					if len(channel) == cap(channel) {
+						<-channel
+					}
+					channel <- message
+				}
 
-           }
-       }
+			}
+		}
 	}
-    return 
+	return
 }
 
-func readFromClient(conn net.Conn, serverRead chan string, 
-    clientExit chan bool){
-    var buf [2048]byte
-    for{
-        if n, err := conn.Read(buf[:]); err == io.EOF{
-            clientExit <- true
-            serverRead <- "die"
-        }else{
-            serverRead <- string(buf[0,n])
-        }
-    }
+func readFromClient(conn net.Conn, serverRead chan string,
+	clientExit chan bool) {
+	var buf [2048]byte
+	for {
+		if n, err := conn.Read(buf[:]); err == io.EOF {
+			fuck("cyan", "Detect one client is DOWN.")
+			serverRead <- "die"
+			clientExit <- true
+		} else {
+			serverRead <- string(buf[0:n])
+		}
+	}
 }
 
 func (mes *multiEchoServer) deCount() {
@@ -173,3 +210,20 @@ func (mes *multiEchoServer) Count() int {
 }
 
 // TODO: add additional methods/functions below!
+
+func fuck(color string, content string) {
+	if logger != nil {
+		logger.Output(2, colorText(color, content))
+	}
+}
+func colorText(color string, content string) string {
+	var code int
+	if color == "cyan" {
+		code = 36
+	} else if color == "red" {
+		code = 31
+	} else {
+		code = 37
+	}
+	return fmt.Sprintf("\x1b[%vm%s\x1b[0m", code, content)
+}
